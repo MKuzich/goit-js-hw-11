f65fa7736d0a5d5f661fa1232fc8cbad590a47dd
Mykhailo Kuzich <timmkedth@gmail.com>
2022-06-14 22:11:15 +0300

    fethc func, markup funk, import simplelightbox

--- src/index.js
+++ src/index.js
@@ -1,31 +1,68 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from "simplelightbox";
+
 const form = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
+let page = 1;
 
 form.addEventListener('submit', onFormSubmit);
+form.addEventListener('change', onFormChange);
 
 function onFormSubmit(event) {
     event.preventDefault();
-
     const name = event.currentTarget.elements.searchQuery.value.trim();
 
-    console.log(name);
-
-
-    fetchImages(name)
-
+    fetchImages(name);
 }
 
-// https://pixabay.com/api/?key=27957885-8dff7fee3c243073fce7c6825&q=yellow+flowers&image_type=photo
+function onFormChange() {
+    page = 1;
+    gallery.innerHTML = "";
+}
 
 function fetchImages(request) {
-
-    
     const API_KEY = '27957885-8dff7fee3c243073fce7c6825';
     const IMAGE_TYPE = 'photo';
     const ORIENTATION = 'horizontal';
     const SAFE_SEARCH = true;
+    const url = `https://pixabay.com/api/?key=${API_KEY}&q=${request}&image_type=${IMAGE_TYPE}&orientation=${ORIENTATION}&safesearch=${SAFE_SEARCH}&per_page=40&page=${page}`;
 
-    const url = `https://pixabay.com/api/?key=${API_KEY}&q=${request}&image_type=${IMAGE_TYPE}&orientation=${ORIENTATION}&safesearch=${SAFE_SEARCH}`;
-
-    console.log(url)
-    fetchImages(url)
+    return fetch(url)
+        .then(r => r.json())
+        .then(r => createImagesListMarkup(r.hits))
+        .then(r => gallery.insertAdjacentHTML('beforeend', r))
+        .then(() => {const links = gallery.querySelectorAll('.gallery__item');
+        links.forEach(el => el.addEventListener('click', e => e.preventDefault()));
+        let lightbox = new SimpleLightbox('.gallery__item', {captionDelay: "250"});})
+        .then(page += 1)
+        .catch(console.log);
 }
+
+function createImagesListMarkup(items) {
+    return items.map(({likes, views, comments, downloads, tags, webformatURL, largeImageURL}) => {
+        return `
+  <div class="photo-card">
+  <a href="${largeImageURL}" class="gallery__item">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  </a>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>${likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b>${views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b>${comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>${downloads}
+    </p>
+  </div>
+</div>
+  `})
+  .join("")
+  }
+
+//   &hits=webformatURL,largeImageURL,tags,likes,views,comments,downloads
