f50524039de6ee249067f00a6f1287d4a77b9e59
Mykhailo Kuzich <timmkedth@gmail.com>
2022-06-18 22:07:14 +0300

    chged fetch func, messages

--- src/index.js
+++ src/index.js
@@ -1,45 +1,85 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 const form = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
 let page = 1;
+let summaryHits = 0;
+
+// const { height: cardHeight } = document
+// .querySelector(".gallery")
+// .firstElementChild.getBoundingClientRect();
+
+// window.scrollBy({
+//   top: cardHeight * 2,
+//   behavior: "smooth",
+// });
 
 form.addEventListener('submit', onFormSubmit);
 form.addEventListener('change', onFormChange);
+loadMoreBtn.addEventListener('click', onLoadMoreBtnClick);
+
+function onLoadMoreBtnClick() {
+
+}
 
 function onFormSubmit(event) {
-    event.preventDefault();
-    loadMoreBtn.classList.add('is-hidden');
-    const name = event.currentTarget.elements.searchQuery.value.trim();
+  event.preventDefault();
+  loadMoreBtn.classList.add('is-hidden');
+  const name = event.currentTarget.elements.searchQuery.value.trim();
 
-    fetchImages(name);
+  if (summaryHits > 500) {
+    Notify.warning("We're sorry, but you've reached the end of search results.")
+    loadMoreBtn.classList.add('is-hidden');
+    return;
+  }
+
+  fetchImages(name)
+    .then(r => {
+      if (r.hits.length === 0) {
+        throw new Error('Sorry, there are no images matching your search query. Please try again.');
+      } else {
+        console.log(page);
+        if (page === 1) {
+          Notify.success(`Hooray! We found ${r.totalHits} images.`);
+        };
+        return createImagesListMarkup(r.hits)
+      }
+    })
+    .then(r => gallery.insertAdjacentHTML('beforeend', r))
+    .then(() => {const links = gallery.querySelectorAll('.gallery__item');
+    links.forEach(el => el.addEventListener('click', e => e.preventDefault()));
+    let lightbox = new SimpleLightbox('.photo-card a', {captionDelay: "250"});
+    loadMoreBtn.classList.remove('is-hidden');
+    summaryHits = page * 40;
+    page += 1;
+    })
+    .catch((error) => Notify.failure(error));
 }
 
 function onFormChange() {
     page = 1;
+    summaryHits = 0;
     gallery.innerHTML = "";
 }
 
 function fetchImages(request) {
-    const API_KEY = '27957885-8dff7fee3c243073fce7c6825';
-    const IMAGE_TYPE = 'photo';
-    const ORIENTATION = 'horizontal';
-    const SAFE_SEARCH = true;
-    const url = `https://pixabay.com/api/?key=${API_KEY}&q=${request}&image_type=${IMAGE_TYPE}&orientation=${ORIENTATION}&safesearch=${SAFE_SEARCH}&per_page=40&page=${page}`;
+  const API_KEY = '27957885-8dff7fee3c243073fce7c6825';
+  const IMAGE_TYPE = 'photo';
+  const ORIENTATION = 'horizontal';
+  const SAFE_SEARCH = true;
+  const url = `https://pixabay.com/api/?key=${API_KEY}&q=${request}&image_type=${IMAGE_TYPE}&orientation=${ORIENTATION}&safesearch=${SAFE_SEARCH}&per_page=40&page=${page}`;
 
-    return fetch(url)
-        .then(r => r.json())
-        .then(r => createImagesListMarkup(r.hits))
-        .then(r => gallery.insertAdjacentHTML('beforeend', r))
-        .then(() => {const links = gallery.querySelectorAll('.gallery__item');
-        links.forEach(el => el.addEventListener('click', e => e.preventDefault()));
-        let lightbox = new SimpleLightbox('.photo-card a', {captionDelay: "250"});
-        loadMoreBtn.classList.remove('is-hidden');
-        page += 1})
-        .catch(Notify.failure('Sorry, there are no images matching your search query. Please try again.'));
-}
+  return fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    });
+};
 
 function createImagesListMarkup(items) {
     return items.map(({likes, views, comments, downloads, tags, webformatURL, largeImageURL}) => {
@@ -67,4 +107,4 @@ function createImagesListMarkup(items) {
   .join("")
   }
 
-//   &hits=webformatURL,largeImageURL,tags,likes,views,comments,downloads
+
