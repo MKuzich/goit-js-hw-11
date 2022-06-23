d2b95a67b4c3bc3edc403f910fbd989dbb0b70a8
Mykhailo Kuzich <timmkedth@gmail.com>
2022-06-23 22:53:00 +0300

    functions separated at diff files

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import throttle from 'lodash.throttle';
 import debounce from 'lodash.debounce';
-import axios from 'axios';
+import { fetchImages, resetCounters, increaseCounters } from './js/fetchImages';
+import { createImagesListMarkup } from './js/createImagesListMarkup';
+import { makeSmoothScroll } from './js/pageScrolling';
 
 const form = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
@@ -12,8 +14,6 @@ const clientScreenHeight = document.documentElement.clientHeight + 200;
 const DELAY = 250;
 const debounceOptions = {leading: true, trailing: false};
 
-let page = 1;
-let summaryHits = 0;
 let name = '';
 let simpleGallery = null;
 
@@ -83,25 +83,6 @@ function refreshSimpleGallery() {
   simpleGallery.refresh();
 };
 
-function increaseCounters() {
-  summaryHits = page * 40;
-  page += 1;
-};
-
-function resetCounters() {
-  page = 1;
-  summaryHits = 0;
-};
-
-function makeSmoothScroll() {
-  const { height: cardHeight } = document.querySelector(".gallery").firstElementChild.getBoundingClientRect();
-
-  window.scrollBy({
-    top: cardHeight * 2,
-    behavior: "smooth",
-  });
-};
-
 function onFormInput() {
   gallery.innerHTML = "";
   resetCounters();
@@ -116,56 +97,9 @@ function requestChange(event) {
   name = event.currentTarget.elements.searchQuery.value.trim();
 };
 
-function fetchImages(request) {
-  const searchParams = new URLSearchParams({
-    key: '27957885-8dff7fee3c243073fce7c6825',
-    q: request,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    per_page: 40,
-    page: page,
-  });
-
-  const url = `https://pixabay.com/api/?${searchParams}`;
-
-  return fetch(url)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    });
-};
 
 function drawImages(imagesArray) {
   return gallery.insertAdjacentHTML('beforeend', createImagesListMarkup(imagesArray))
-};
-
-function createImagesListMarkup(items) {
-    return items.map(({likes, views, comments, downloads, tags, webformatURL, largeImageURL}) => {
-        return `
-  <div class="photo-card">
-  <a href="${largeImageURL}" class="gallery__item">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-  </a>
-  <div class="info">
-    <p class="info-item">
-      <b>Likes</b><span>${likes}</span>
-    </p>
-    <p class="info-item">
-      <b>Views</b><span>${views}</span>
-    </p>
-    <p class="info-item">
-      <b>Comments</b><span>${comments}</span>
-    </p>
-    <p class="info-item">
-      <b>Downloads</b><span>${downloads}</span>
-    </p>
-  </div>
-</div>
-  `})
-  .join("")
 };
 
 
---
+++ src/js/fetchImages.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+let page = 1;
+let summaryHits = 0;
+
+export function increaseCounters() {
+    summaryHits = page * 40;
+    page += 1;
+};
+
+export function resetCounters() {
+    page = 1;
+    summaryHits = 0;
+};
+
+export function fetchImages(request) {
+    const searchParams = new URLSearchParams({
+        key: '27957885-8dff7fee3c243073fce7c6825',
+        q: request,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        per_page: 40,
+        page: page
+    });
+
+    const url = `https://pixabay.com/api/?${searchParams}`;
+
+    return fetch(url)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(response.status);
+        }
+        return response.json();
+    });
+};
